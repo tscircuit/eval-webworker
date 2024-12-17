1f6fd8cf7c8575f8f84b4a97da1b435ea8699b25
Rishabh Gupta <belikerishabh@gmail.com>
2024-12-17 19:02:25 +0530

    feat: Emit error

--- webworker/eval-compiled-js.ts
+++ webworker/eval-compiled-js.ts
@@ -1,22 +1,33 @@
 export function evalCompiledJs(
   compiledCode: string,
   preSuppliedImports: Record<string, any>,
+  ctx: { emitError?: (error: { message: string, stack?: string }) => void } = {}
 ) {
-  ;(globalThis as any).__tscircuit_require = (name: string) => {
-    if (name.startsWith("./") && preSuppliedImports[name.slice(2)]) {
-      return preSuppliedImports[name.slice(2)]
+  try {
+    ;(globalThis as any).__tscircuit_require = (name: string) => {
+      if (name.startsWith("./") && preSuppliedImports[name.slice(2)]) {
+        return preSuppliedImports[name.slice(2)]
+      }
+      if (!preSuppliedImports[name]) {
+        throw new Error(`Import "${name}" not found`)
+      }
+      return preSuppliedImports[name]
     }
-    if (!preSuppliedImports[name]) {
-      throw new Error(`Import "${name}" not found`)
+    const functionBody = `
+      var exports = {};
+      var require = globalThis.__tscircuit_require;
+      var module = { exports };
+      var circuit = globalThis.__tscircuit_circuit;
+      ${compiledCode};
+      return module;`.trim()
+    return Function(functionBody).call(globalThis)
+  } catch (error: any) {
+    if (ctx.emitError) {
+      ctx.emitError({
+        message: error.message,
+        stack: error.stack
+      })
     }
-    return preSuppliedImports[name]
+    throw error
   }
-  const functionBody = `
-    var exports = {};
-    var require = globalThis.__tscircuit_require;
-    var module = { exports };
-    var circuit = globalThis.__tscircuit_circuit;
-    ${compiledCode};
-    return module;`.trim()
-  return Function(functionBody).call(globalThis)
 }
